af7155e7712eae0c2bed71c5ad681c22364883a2
Willie Conaway <YOUR_GITHUB_EMAIL_HERE>
2026-04-29 10:49:46 -0400

    Update, Finalizing and Removing Net + and Sec + removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+const Router = window.location.protocol === 'file:' ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
